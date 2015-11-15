9890f1a46517625ba009eacbbb8c4b37012d156c
Tatsuya Nanjo <noralife@gmail.com>
2015-11-15 12:08:17 +0900

    Change port

--- test_grape_api.sh
+++ test_grape_api.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-SAHARA_CMS_URL=http://localhost:9393 SAHARA_PMS_URL=http://localhost:9494 SAHARA_OMS_URL=http://localhost:9595 rspec spec/spec_order.rb spec/spec_product.rb spec/spec_customer.rb
+SAHARA_CMS_URL=http://localhost:30301 SAHARA_PMS_URL=http://localhost:30302 SAHARA_OMS_URL=http://localhost:30303 rspec spec/spec_order.rb spec/spec_product.rb spec/spec_customer.rb
+SAHARA_URL=http://localhost:8000 rspec spec/spec_webui.rb
